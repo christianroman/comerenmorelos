9f0a2ff50b2e4470f2c9a9f11541a921c710e775
Christian Roman <chroman16@gmail.com>
2013-10-08 00:35:13 -0500

    API

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -17,14 +17,14 @@ module Api
 
           if @guest.save
 
-            @contact = Contact.new(room_id: params[:room_id].blank? ? nil : params[:room_id] == 0 ? nil : params[:room_id], guest_id: @guest.id, check_in: params[:check_in], check_out: params[:check_out], adults: params[:adults], children: params[:children], comment: params[:comment], restaurant_id: params[:restaurant_id])
+            @contact = Contact.new(guest_id: @guest.id, check_in: params[:check_in], adults: params[:adults], comment: params[:comment], restaurant_id: params[:restaurant_id])
 
             if @contact.valid?
 
               if @contact.save
 
                 if Rails.env.production?
-                  ActionMailer::Base.mail(:from => 'turestaurantenmorelos@gmail.com', :to => Restaurant.find(params[:restaurant_id]).email, :subject => 'Nueva solicitud de informacion', :body => 'Se ha realizado una nueva solicitud de informacion a traves del portal. Favor de revisar').deliver
+                  ActionMailer::Base.mail(:from => 'comerenmorelos@gmail.com', :to => Restaurant.find(params[:restaurant_id]).email, :subject => 'Nueva solicitud de informacion', :body => 'Se ha realizado una nueva solicitud de informacion a traves del portal. Favor de revisar').deliver
                 end
 
                 format.json { render :json => { :status => 'OK' }, status: :created }
